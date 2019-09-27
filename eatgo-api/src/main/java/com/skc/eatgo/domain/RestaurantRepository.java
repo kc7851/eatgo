be936f2a77df5376042d13eb47505dab1c4d623b
skc <kc7851@gmail.com>
2019-09-27 17:30:35 +0900

    Chapter04-10.의존성주입
    Controller에서 직접 Repository 객체를 생성하던 것을 @Autowired를 통해 스프링에게 DI받음.
    Repository를 interface로 분리하고 구현체를 따로 만들어서 결합도는 낮춤.

--- eatgo-api/src/main/java/com/skc/eatgo/domain/RestaurantRepository.java
+++ eatgo-api/src/main/java/com/skc/eatgo/domain/RestaurantRepository.java
@@ -1,25 +1,11 @@
 package com.skc.eatgo.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class RestaurantRepository {
+public interface RestaurantRepository {
 
-    private List<Restaurant> restaurants = new ArrayList<>();
+    List<Restaurant> findAll();
 
-    public RestaurantRepository() {
-        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
-    }
+    Restaurant findById(Long id);
 
-    public List<Restaurant> findAll() {
-        return restaurants;
-    }
-
-    public Restaurant findById(Long id) {
-        return restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-    }
 }
--- eatgo-api/src/test/java/com/skc/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/com/skc/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,14 +1,16 @@
 package com.skc.eatgo.interfaces;
 
+import com.skc.eatgo.domain.RestaurantRepository;
+import com.skc.eatgo.domain.RestaurantRepositoryImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -20,6 +22,9 @@ public class RestaurantControllerTest {
 
     @Autowired
     MockMvc mvc;
+
+    @SpyBean(RestaurantRepositoryImpl.class)
+    private RestaurantRepository restaurantRepository;
 
     @Test
     public void list() throws Exception {
