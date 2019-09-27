676b56f6514cd93d04139102db69c95e48ff8e76
skc <kc7851@gmail.com>
2019-09-27 15:18:20 +0900

    Chapter03-01.가게목록

--- eatgo-api/src/main/java/com/skc/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/com/skc/eatgo/domain/Restaurant.java
@@ -2,12 +2,18 @@ package com.skc.eatgo.domain;
 
 public class Restaurant {
 
+    private final Long id;
     private final String name;
     private final String address;
 
-    public Restaurant(String name, String address) {
+    public Restaurant(Long id, String name, String address) {
+        this.id = id;
         this.name = name;
         this.address = address;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
@@ -21,4 +27,5 @@ public class Restaurant {
     public String getAddress() {
         return address;
     }
+
 }
