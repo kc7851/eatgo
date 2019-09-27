bc590ebd2e02e10f16ce147893cb0390f2710a6d
skc <kc7851@gmail.com>
2019-09-27 13:18:05 +0900

    Chapter02-02.TDD
    Restaurant 클래스 생성
    Test 코드 작성

---
+++ eatgo-api/src/test/java/com/skc/eatgo/domain/RestaurantTest.java
@@ -0,0 +1,23 @@
+package com.skc.eatgo.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class RestaurantTest {
+
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("Seoul"));
+    }
+
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
+}
