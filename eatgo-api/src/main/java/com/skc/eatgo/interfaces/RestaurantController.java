ac2af62a22f5bffc7e86282fffbabdfaec7d1aed
skc <kc7851@gmail.com>
2019-09-27 15:50:48 +0900

    Chapter03-02.가게상세-1

--- eatgo-api/src/main/java/com/skc/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/skc/eatgo/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package com.skc.eatgo.interfaces;
 
 import com.skc.eatgo.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,4 +22,18 @@ public class RestaurantController {
         return restaurants;
     }
 
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detatil(@PathVariable("id") Long id) {
+        List<Restaurant> restaurants = new ArrayList<>();
+
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+
+        Restaurant restaurant = restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        return restaurant;
+    }
 }
