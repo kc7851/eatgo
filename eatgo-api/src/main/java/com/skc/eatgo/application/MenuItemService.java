ef4a6b20953c807cd4b7b46d69d2e38baa79ddbd
skc <kc7851@gmail.com>
2019-10-18 16:20:26 +0900

    23. 메뉴 관리 - 1

---
+++ eatgo-api/src/main/java/com/skc/eatgo/application/MenuItemService.java
@@ -0,0 +1,14 @@
+package com.skc.eatgo.application;
+
+import com.skc.eatgo.domain.MenuItem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+    public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems) {
+
+    }
+}
