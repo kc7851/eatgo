4dffe743f31ed29a7eedac783cf06dcc48048ece
skc <kc7851@gmail.com>
2019-10-18 17:38:45 +0900

    25. 메뉴 관리 - 3

--- eatgo-api/src/test/java/com/skc/eatgo/application/MenuItemServiceTests.java
+++ eatgo-api/src/test/java/com/skc/eatgo/application/MenuItemServiceTests.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -35,11 +36,13 @@ public class MenuItemServiceTests {
         List<MenuItem> menuItems = new ArrayList<>();
 
         menuItems.add(MenuItem.builder().name("Kimchi").build());
-        menuItems.add(MenuItem.builder().name("KukBob").build());
+        menuItems.add(MenuItem.builder().id(12L).name("KukBob").build());
+        menuItems.add(MenuItem.builder().id(1004L).destroy(true).build());
 
         menuItemService.bulkUpdate(1L, menuItems);
 
         verify(menuItemRepository, times(2)).save(any());
+        verify(menuItemRepository, times(1)).deleteById(eq(1004L));
     }
 
 }
