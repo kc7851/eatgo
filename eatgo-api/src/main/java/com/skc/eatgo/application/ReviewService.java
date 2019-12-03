9c387e46c712bee51987a7ba954394a7beaf4923
KiChang Shin <kc7851@gmail.com>
2019-12-03 20:39:17 +0900

    26. 리뷰 작성 - 2

--- eatgo-api/src/main/java/com/skc/eatgo/application/ReviewService.java
+++ eatgo-api/src/main/java/com/skc/eatgo/application/ReviewService.java
@@ -1,11 +1,21 @@
 package com.skc.eatgo.application;
 
 import com.skc.eatgo.domain.Review;
+import com.skc.eatgo.domain.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
-    public void addReview(Review review) {
-        // TODO;
+
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public Review addReview(Review review) {
+        return reviewRepository.save(review);
     }
 }
--- eatgo-api/src/main/java/com/skc/eatgo/domain/Review.java
+++ eatgo-api/src/main/java/com/skc/eatgo/domain/Review.java
@@ -2,19 +2,34 @@ package com.skc.eatgo.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class Review {
 
     @Id
     @GeneratedValue
-    Long id;
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @Max(5)
+    @Min(0)
+    private Integer score;
+
+    @NotEmpty
+    private String description;
 }
