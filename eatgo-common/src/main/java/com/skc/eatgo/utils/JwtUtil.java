80bf2a7a3fe731f4057a9c209b601b68f2cc1ca6
KiChang Shin <kc7851@gmail.com>
2019-12-29 21:43:42 +0900

    54. 인가 - 1

--- eatgo-common/src/main/java/com/skc/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/com/skc/eatgo/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.skc.eatgo.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -16,13 +17,17 @@ public class JwtUtil {
     }
 
     public String createToken(long userId, String name) {
-        String token = Jwts.builder()
+        return Jwts.builder()
                 .claim("userId", userId)
                 .claim("name", name)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
-
-        return token;
     }
 
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
---
+++ eatgo-customer-api/src/main/java/com/skc/eatgo/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.skc.eatgo.filters;
+
+import com.skc.eatgo.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+
+    private JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(
+            AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        super(authenticationManager);
+        this.jwtUtil = jwtUtil;
+    }
+
+    //TODO: 실제로 JWT 분석 필요함.
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain)
+    throws IOException, ServletException {
+        Authentication authentication = getAuthentication(request);
+
+        if (authentication != null) {
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private Authentication getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token == null) {
+            return null;
+        }
+
+        Claims claims = jwtUtil.getClaims(token.substring("Bearer".length()));
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(claims, null);
+        return authentication;
+    }
+}
